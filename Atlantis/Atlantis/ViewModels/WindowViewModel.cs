71798e52e4a1f37fcd63813c23914fa4032a7d6f
Nigel Tatschner <ntatschner@gmail.com>
2017-11-09 00:50:25 +0000

    .

---
+++ Atlantis/Atlantis/ViewModels/WindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Atlantis
+{
+    /// <summary>
+    /// View modal for the main windows frame
+    /// </summary>
+    public class WindowViewModel : BaseViewModel
+    {
+        #region Private Variable
+        /// <summary>
+        /// The windows this view model controls 
+        /// </summary>
+        private Window mWindow;
+
+        #endregion
+
+        #region Public Properties
+
+        public string Test { get; set; } = "Test Binding!";
+        
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+        }
+
+        #endregion
+    }
+}

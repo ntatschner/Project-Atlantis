ee1ded71b03e5b3d0bffb73ab04efa35623e0df1
Nigel Tatschner <ntatschner@gmail.com>
2017-11-13 23:09:24 +0000

    UI Updates

--- Atlantis/Atlantis/ViewModels/Base/BaseViewModel.cs
+++ Atlantis/Atlantis/ViewModels/Base/BaseViewModel.cs
@@ -13,5 +13,13 @@ namespace Atlantis
         /// The event that is fired when any child property changes its value
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
--- Atlantis/Atlantis/ViewModels/WindowViewModel.cs
+++ Atlantis/Atlantis/ViewModels/WindowViewModel.cs
@@ -8,22 +8,86 @@ using System.Windows;
 namespace Atlantis
 {
     /// <summary>
-    /// View modal for the main windows frame
+    /// View model for the main windows frame
     /// </summary>
     public class WindowViewModel : BaseViewModel
     {
-        #region Private Variable
+        #region Private Member
         /// <summary>
         /// The windows this view model controls 
         /// </summary>
         private Window mWindow;
 
+        /// <summary>
+        /// The Margin around the window to allow for drop shadow
+        /// </summary>
+        private int mOuterMarginSize = 5;
+
+        /// <summary>
+        /// The radius of the edges of the window
+        /// </summary>
+        private int mWindowRadius = 5;
+
         #endregion
 
         #region Public Properties
 
-        public string Test { get; set; } = "Test Binding!";
-        
+        /// <summary>
+        /// This is the size of the border around the edges of the main window
+        /// </summary>
+        public int ResizeBorder { get; set; } = 3;
+
+        /// <summary>
+        /// The Size of the resize border around the window, taking into account the outermargin
+        /// </summary>
+        public Thickness ResizeBorderThickness { get { return new Thickness(ResizeBorder + OuterMarginSize); } }
+
+        /// <summary>
+        /// The outer margin size of the window to allow for a drop shadow
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mOuterMarginSize;
+            }
+            set
+            {
+                mOuterMarginSize = value;
+            }
+        }
+
+        /// <summary>
+        /// The outer margin Thickness size of the window to allow for a drop shadow
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get { return new Thickness(OuterMarginSize);  } }
+
+        /// <summary>
+        /// The windows radius at the edges of the window
+        /// </summary>
+        public int WindowRadius
+        {
+            get
+            {
+                return mWindow.WindowState == WindowState.Maximized ? 0 : mWindowRadius;
+            }
+            set
+            {
+                mWindowRadius = value;
+            }
+        }
+
+        /// <summary>
+        /// The windows radius at the edges of the window
+        /// </summary>
+        public CornerRadius WindowCornerRadius { get { return new CornerRadius(WindowRadius); } }
+
+        /// <summary>
+        /// The height of the title bar option of the windows
+        /// </summary>
+        public int TitleHeight { get; set; } = 35;
+
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
 
         #endregion
 
@@ -34,6 +98,17 @@ namespace Atlantis
         public WindowViewModel(Window window)
         {
             mWindow = window;
+
+            //listen out for the window resizing
+            mWindow.StateChanged += (sender, e) =>
+            {
+                //fire off events for all properties that are affected by a resize
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
         }
 
         #endregion
